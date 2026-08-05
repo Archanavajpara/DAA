bfd6d03143aa42a31139c9f07c153a9dc24e9d7b
Archana_Vajpara <arvajpara1806@gmail.com>
2026-08-05 17:30:04 +0530

    DAA

--- Lab7/krushkal.c
+++ Lab7/krushkal.c
@@ -1,1 +1,70 @@
-// krushkal
+#include <stdio.h>
+
+#define Max 100
+
+struct edge{
+    int u;
+    int v;
+    int weight;
+};
+
+struct edge e[Max];
+
+int parent[Max];
+
+int find(int vertex){
+    while(parent[vertex] != vertex){
+        vertex = parent[vertex];
+    }
+    return vertex;
+}
+
+void unionSet(int u,int v){
+    int ParentU = find(u);
+    int ParentV = find(v);
+    parent[ParentU] = ParentV;
+}
+
+int main(){
+
+    int vertex, edge;
+
+    scanf("%d %d",&vertex,&edge);
+
+    for(int i=0;i<edge;i++){
+        scanf("%d %d %d",&e[i].u,&e[i].v,&e[i].weight);
+    }
+
+    for(int i=0;i<vertex;i++){
+        parent[i]=i;
+    }
+
+    for(int i=0;i<edge-1;i++){
+        for(int j=0;j<edge-i-1;j++){
+            if(e[j].weight > e[j+1].weight){
+                struct edge temp = e[j];
+                e[j] = e[j+1];
+                e[j+1] = temp;
+            }
+        }
+    }
+
+    int totalCost=0;
+
+    for(int i=0;i<edge;i++){
+
+        int u = e[i].u;
+        int v = e[i].v;
+        int weight = e[i].weight;
+
+        if(find(u) != find(v)){
+            unionSet(u,v);
+            totalCost += weight;
+            printf("Edge (%d, %d) cost = %d\n",u,v,weight);
+        }
+    }
+
+    printf("Total Cost = %d\n",totalCost);
+
+    return 0;
+}
--- Lab7/prims.c
+++ Lab7/prims.c
@@ -1,1 +1,70 @@
-// prims
+#include<stdio.h>
+#include<stdbool.h>
+#include<limits.h>
+
+#define Max 100
+
+int main()
+{
+    int vertex;
+    scanf("%d", &vertex);
+
+    int graph[Max][Max];
+    bool visited[Max] = {false};
+
+    for(int i=0;i<vertex;i++)
+    {
+        for(int j=0;j<vertex;j++)
+        {
+            scanf("%d",&graph[i][j]);
+        }
+    }
+
+    visited[0] = true;
+
+    int EdgeCount = 0;
+    int totalCost = 0;
+
+    while(EdgeCount < vertex-1)
+    {
+        int min = INT_MAX;
+        int source = -1;
+        int destination = -1;
+
+        for(int i=0;i<vertex;i++)
+        {
+            if(visited[i])
+            {
+                for(int j=0;j<vertex;j++)
+                {
+                    if(!visited[j] && graph[i][j] != 0)
+                    {
+                        if(graph[i][j] < min)
+                        {
+                            min = graph[i][j];
+                            source = i;
+                            destination = j;
+                        }
+                    }
+                }
+            }
+        }
+
+        if(destination == -1)
+        {
+            printf("Graph is not connected\n");
+            return 0;
+        }
+
+        printf("Edge %d: (%d, %d) cost: %d\n",
+               EdgeCount + 1, source, destination, min);
+
+        visited[destination] = true;
+        totalCost += min;
+        EdgeCount++;
+    }
+
+    printf("Minimum Cost = %d\n", totalCost);
+
+    return 0;
+}
